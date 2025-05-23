b9dba8bb57b19e7ce81aab9e1efa7921566266ce
OH-JINHYEON <jinibrighteveryday@gmail.com>
2025-05-23 17:09:01 +0900

    Feat: IntrosuceMyself 프로젝트 시작

---
+++ IntroduceMyself.java
@@ -0,0 +1,7 @@
+public class IntroduceMyself {
+    // 이름, 나이, 관심사를 입력하면
+    //자기소개 문장을 만들어 출력해주는 프로그램
+    public static void main(String[] args) { // 입력받고 메서드 호출하기
+        // 소개문 생성 및 반환하기
+    }
+}
